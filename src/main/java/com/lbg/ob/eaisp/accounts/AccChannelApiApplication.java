008427bebc52ae96b1e25307b509a183a7d84f7c
Naga Advik Reddy <dskkumardsk3@gmail.com>
2025-06-03 17:13:12 +0530

    initial commit

---
+++ src/main/java/com/lbg/ob/eaisp/accounts/AccChannelApiApplication.java
@@ -0,0 +1,13 @@
+package com.lbg.ob.eaisp.accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccChannelApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccChannelApiApplication.class, args);
+    }
+
+}
